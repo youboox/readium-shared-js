cdd3eadcc325e9a5ac02e906c0baad06cc656370
Juan Corona <juanc@evidentpoint.com>
2018-02-14 12:12:21 -0800

    Update typings

--- dev/readium-shared-js.d.ts
+++ dev/readium-shared-js.d.ts
@@ -1,6 +1,7 @@
 declare module 'readium-shared-js' {
   export var ReaderView: any;
   export var ReflowableView: any;
+  export var OnePageView: any;
   export var Package: any;
   export var ViewerSettings: any;
   export var StyleCollection: any;
