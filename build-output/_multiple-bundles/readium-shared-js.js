3cf32587218da6be779c62fac630cf072ebaccd1
vince4 <vincentmeyer.etu@gmail.com>
2019-02-15 18:03:18 +0100

    UPDATE multiple_bundles build

--- build-output/_multiple-bundles/readium-shared-js.js
+++ build-output/_multiple-bundles/readium-shared-js.js
@@ -16826,6 +16826,21 @@ var ReflowableView = function(options, reader){
             return paginationInfo;
         }
 
+        // DEBUG Recalculate columnCount before sending paginationInfo
+        var dim = (_htmlBodyIsVerticalWritingMode ? _$epubHtml[0].scrollHeight : _$epubHtml[0].scrollWidth);
+        if (dim == 0) {
+            console.error("Document dimensions zero?!");
+            return paginationInfo;
+        }
+
+        _paginationInfo.columnCount = (dim + _paginationInfo.pageOffset + _paginationInfo.columnGap) / (_paginationInfo.columnWidth + _paginationInfo.columnGap);
+        _paginationInfo.columnCount = Math.round(_paginationInfo.columnCount);
+        if (_paginationInfo.columnCount == 0 || _paginationInfo.columnWidth == 0) {
+            console.error("Column count zero?!");
+            return paginationInfo;
+        }
+        // END Recalculate columnCount before sending paginationInfo
+
         var pageIndexes = getOpenPageIndexes();
 
         for(var i = 0, count = pageIndexes.length; i < count; i++) {
@@ -16836,6 +16851,21 @@ var ReflowableView = function(options, reader){
         return paginationInfo;
 
     };
+
+    function getOpenPageIndexes() {
+
+        var indexes = [];
+
+        var currentPage = _paginationInfo.currentSpreadIndex * _paginationInfo.visibleColumnCount;
+
+        for(var i = 0; i < _paginationInfo.visibleColumnCount && (currentPage + i) < _paginationInfo.columnCount; i++) {
+
+            indexes.push(currentPage + i);
+        }
+
+        return indexes;
+
+    }
 
     function getOpenPageIndexes() {
 
